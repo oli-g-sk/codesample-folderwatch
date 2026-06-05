e0ddb0676d18ce75b9a23f67199118c067c98a16
Oliver <oliver.g.sk@gmail.com>
2026-06-05 15:47:45 +0200

    FolderContents static load from file system

--- ServerFolderWatch.Core/FolderContents.cs
+++ ServerFolderWatch.Core/FolderContents.cs
@@ -1,10 +1,34 @@
 using System.Collections;
+using System.IO.Abstractions;
 
 namespace ServerFolderWatch.Core;
 
 public class FolderContents
 {
-    public IList<string> Subfolders { get; } = new List<string>();
+    private FolderContents()
+    {
+    }
     
-    public IDictionary<string, int> VersionedFiles { get; } = new Dictionary<string, int>();
+    public IList<string> Subfolders { get; private set; } = new List<string>();
+    
+    public IDictionary<string, int> VersionedFiles { get; private set; } = new Dictionary<string, int>();
+
+    public static FolderContents Empty { get; } = new();
+
+    public static FolderContents FromFolder(string folderPath, IDirectory directory, IPath path)
+    {
+        var subfolders = directory.EnumerateDirectories(folderPath)
+            .Select(path.GetDirectoryName).OfType<string>();
+
+        var files = directory.EnumerateFiles(folderPath)
+            .Select(path.GetFileName).OfType<string>();
+        
+        var versionedFiles = files.ToDictionary(x => x, x => 1);
+
+        return new FolderContents
+        {
+            Subfolders = subfolders.ToList(),
+            VersionedFiles = versionedFiles
+        };
+    }
 }
