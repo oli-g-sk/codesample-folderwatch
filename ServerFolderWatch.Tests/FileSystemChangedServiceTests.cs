5ccb4707aa358e4773301ef4a8b242545321e789
Oliver <oliver.g.sk@gmail.com>
2026-06-05 12:44:36 +0200

    Added test project

---
+++ ServerFolderWatch.Tests/FileSystemChangedServiceTests.cs
@@ -0,0 +1,21 @@
+﻿namespace ServerFolderWatch.Tests;
+
+public class FileSystemChangedServiceTests
+{
+    [Fact]
+    public void Setup_ThrowsForInvalidPath()
+    {
+        
+    }
+    
+    [Fact]
+    public void Setup_ThrowsIfAlreadySetup()
+    {
+        
+    }
+    
+    [Fact]
+    public void Setup_CreatesSidecarFile()
+    {
+    }
+}
