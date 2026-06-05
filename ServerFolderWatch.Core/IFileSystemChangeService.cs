fe5a83bb0446cb51732baa1fe0738a8d06870e81
Oliver <oliver.g.sk@gmail.com>
2026-06-05 14:01:16 +0200

    Added doc

--- ServerFolderWatch.Core/IFileSystemChangeService.cs
+++ ServerFolderWatch.Core/IFileSystemChangeService.cs
@@ -2,6 +2,15 @@ namespace ServerFolderWatch.Core;
 
 public interface IFileSystemChangeService
 {
+    /// <summary>
+    /// Set up folder monitoring in the provided <see cref="monitoredPath"/>,
+    /// if not already set up (either directly here, or in a parent folder).
+    /// </summary>
+    /// <param name="monitoredPath"></param>
+    /// <returns>
+    /// True if the folder was just set up for monitoring,
+    /// false if this or a parent folder was already set up in the past.
+    /// </returns>
     Task<bool> Setup(string monitoredPath);
     
     List<string> GetAddedEntries();
