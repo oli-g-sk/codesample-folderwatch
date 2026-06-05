a7d8c6e628ad86b316831a343ed37e589b4f6dce
Oliver <oliver.g.sk@gmail.com>
2026-06-05 12:28:07 +0200

    Init with basic projects

---
+++ ServerFolderWatch.CLI/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ServerFolderWatch.CLI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ ServerFolderWatch.Core/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace ServerFolderWatch.Core;
+
+public class Class1
+{
+}
