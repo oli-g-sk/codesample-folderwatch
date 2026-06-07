b6f28d3b8dcd676d26347b628f34c2cae2ec3aa0
Oliver <oliver.g.sk@gmail.com>
2026-06-07 15:00:04 +0200

    Cleanup of old, invalid tests

--- ServerFolderWatch.Tests/FolderSnapshotServiceTests.cs
+++ ServerFolderWatch.Tests/FolderSnapshotServiceTests.cs
@@ -17,6 +17,8 @@ public class FolderSnapshotServiceTests
 
     private readonly SidecarFileFolderSnapshotService sut;
     
+    private string SidecarFilePath => mockFileSystem.Path.Combine(FolderName, SidecarFileName);
+    
     public FolderSnapshotServiceTests()
     {
         var loggerFactoryMock = new Mock<ILoggerFactory>();
@@ -51,41 +53,24 @@ public class FolderSnapshotServiceTests
             new SidecarFileFolderSnapshotService(new Mock<IFileSystem>().Object,
                 configurationMock.Object, new Mock<ILoggerFactory>().Object));
     }
-
-    [Fact]
-    public void IsFolderAlreadyMonitored_BuildsCorrectSidecarFilePath()
-    {
-        _ = sut.IsFolderAlreadyMonitored(FolderName);
-        
-        // pathMock.Verify(x => x.Combine(FolderPath, SidecarFileName), Times.Once);
-    }
     
     [Theory]
     [InlineData(true)]
     [InlineData(false)]
     public void IsFolderAlreadyMonitored_ReturnsCorrectValue(bool sidecarFileExists)
     {
-        string sidecarFilePath = mockFileSystem.Path.Combine(FolderName, SidecarFileName);
-        
         if (sidecarFileExists)
-        {
-            mockFileSystem.File.Create(sidecarFilePath);
-        }
+            mockFileSystem.File.Create(SidecarFilePath);
 
         bool result = sut.IsFolderAlreadyMonitored(FolderName);
-
         Assert.Equal(sidecarFileExists, result);
     }
 
-    /*
     [Fact]
     public void InitializeFolder_CreatesSidecarFile()
     {
-        fileMock.Setup(x => x.Exists(SidecarFilePath)).Returns(false);
-
-        sut.InitializeFolder(FolderPath, false);
-
-        fileMock.Verify(x => x.Create(SidecarFilePath), Times.Once);
+        sut.InitializeFolder(FolderName, false);
+        Assert.True(mockFileSystem.File.Exists(SidecarFilePath));
     }
 
     [Theory]
@@ -93,11 +78,8 @@ public class FolderSnapshotServiceTests
     [InlineData(false)]
     public void InitializeFolder_SidecarFileAlreadyExists_ThrowsException(bool runRecursively)
     {
-        string sidecarFilePath = TestHelpers.GetPath(FolderPath, SidecarFileName, pathMock);
-
-        fileMock.Setup(x => x.Exists(sidecarFilePath)).Returns(true);
-
-        Assert.Throws<InvalidOperationException>(() => sut.InitializeFolder(FolderPath, runRecursively));
+        mockFileSystem.File.Create(SidecarFilePath);
+        Assert.Throws<InvalidOperationException>(() => sut.InitializeFolder(FolderName, runRecursively));
     }
 
     [Fact]
@@ -109,17 +91,4 @@ public class FolderSnapshotServiceTests
     public void InitializeFolder_RunsRecursively_IfSidecarDoesNotExistInCurrentFolder()
     {
     }
-
-    private void SetupSubfolder()
-    {
-        // mock that subfolder exists
-        directoryMock.Setup(x => x.Exists(SubFolderPath)).Returns(true);
-
-        // mock that subfolder is listed
-        directoryMock.Setup(x => x.GetDirectories(FolderPath)).Returns(new[] {SubFolderPath});
-        directoryMock.Setup(x => x.GetDirectories(SubFolderPath)).Returns(Array.Empty<string>());
-        directoryMock.Setup(x => x.EnumerateDirectories(FolderPath)).Returns(new[] { SubFolderPath });
-        directoryMock.Setup(x => x.EnumerateDirectories(SubFolderPath)).Returns(new[] {SubFolderPath});
-    }
-    */
 }
