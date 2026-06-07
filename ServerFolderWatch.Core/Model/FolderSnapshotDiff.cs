c7619fe53c92b303438edb21048fed316a94724e
Oliver <oliver.g.sk@gmail.com>
2026-06-07 02:25:57 +0200

    Web UI change highlighing

--- ServerFolderWatch.Core/Model/FolderSnapshotDiff.cs
+++ ServerFolderWatch.Core/Model/FolderSnapshotDiff.cs
@@ -5,6 +5,7 @@ namespace ServerFolderWatch.Core.Model;
 /// as a single list containing both past and present items, along with
 /// the diff operation for each.
 /// </summary>
+/// TODO is this class needed, or can the service return list of tuples directly?
 public class FolderSnapshotDiff
 {
     public List<(BaseEntry FileSystemEntry, DiffOperation Operation)> Entries { get; } = new();
