7469383213b2a6706944148252c0ddc542882c9a
Oliver <oliver.g.sk@gmail.com>
2026-06-08 23:36:49 +0200

    Explicit Program class

--- src/ServerFolderWatch.Server/Program.cs
+++ src/ServerFolderWatch.Server/Program.cs
@@ -12,56 +12,62 @@ using ServerFolderWatch.Server;
 using ServerFolderWatch.Server.Components;
 using Testably.Abstractions;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddSingleton<IAppConfiguration>(sp =>
-    builder.Configuration
-        .GetSection("App")
-        .Get<AppConfiguration>() ??
-    throw new InvalidOperationException("Configuration could not be loaded."));
-
-builder.Services.AddSingleton<IFileSystem, RealFileSystem>();
-// TODO use scoped lifecycles?
-builder.Services.AddSingleton<IBrowseService, BrowseService>();
-builder.Services.AddSingleton<IFolderDiffService, FolderDiffService>();
-builder.Services.AddSingleton<IFolderSnapshotService, JsonFolderSnapshotService>();
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
+internal class Program
+{
+    public static void Main(string[] args)
     {
-        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());        
-    });
+        var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
-app.UseStaticFiles();
-app.UseAntiforgery();
-app.MapControllers();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+        builder.Services.AddSingleton<IAppConfiguration>(sp =>
+            builder.Configuration
+                .GetSection("App")
+                .Get<AppConfiguration>() ??
+            throw new InvalidOperationException("Configuration could not be loaded."));
 
-var configuration = app.Services.GetRequiredService<IAppConfiguration>();
-var snapshotService = app.Services.GetRequiredService<IFolderSnapshotService>();
-string rootPublicPath = configuration.RootPublicPath;
+        builder.Services.AddSingleton<IFileSystem, RealFileSystem>();
+// TODO use scoped lifecycles?
+        builder.Services.AddSingleton<IBrowseService, BrowseService>();
+        builder.Services.AddSingleton<IFolderDiffService, FolderDiffService>();
+        builder.Services.AddSingleton<IFolderSnapshotService, JsonFolderSnapshotService>();
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
 
-var browseService = app.Services.GetRequiredService<IBrowseService>();
+        builder.Services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());        
+            });
 
-var logger = app.Services.GetRequiredService<ILogger<Program>>();
+        var app = builder.Build();
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+        app.MapControllers();
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
 
-var fileSystem = app.Services.GetRequiredService<IFileSystem>();
+        var configuration = app.Services.GetRequiredService<IAppConfiguration>();
+        var snapshotService = app.Services.GetRequiredService<IFolderSnapshotService>();
+        string rootPublicPath = configuration.RootPublicPath;
 
-if (!fileSystem.Directory.Exists(rootPublicPath))
-{
-    logger.LogError("Public folder path defined in configuration does not exist: {configurationPath}", rootPublicPath);
-    return;
+        var browseService = app.Services.GetRequiredService<IBrowseService>();
+
+        var logger = app.Services.GetRequiredService<ILogger<Program>>();
+
+        var fileSystem = app.Services.GetRequiredService<IFileSystem>();
+
+        if (!fileSystem.Directory.Exists(rootPublicPath))
+        {
+            logger.LogError("Public folder path defined in configuration does not exist: {configurationPath}", rootPublicPath);
+            return;
+        }
+        if (!browseService.CanWriteToFolder(rootPublicPath))
+        {
+            logger.LogError("Public folder path defined in configuration is not writeable: {configurationPath}", rootPublicPath);
+            return;
+        }
+
+        snapshotService.InitializeFolder(rootPublicPath, true);
+        app.Run();
+    }
 }
-if (!browseService.CanWriteToFolder(rootPublicPath))
-{
-    logger.LogError("Public folder path defined in configuration is not writeable: {configurationPath}", rootPublicPath);
-    return;
-}
-
-snapshotService.InitializeFolder(rootPublicPath, true);
-app.Run();
